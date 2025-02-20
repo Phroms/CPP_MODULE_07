79314c9d8c97dd22bad869b6ae932d42bc30a175
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2025-02-20 18:14:12 +0100

    ex00 terminado

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,7 +1,9 @@
 # pragma once
 # include <iostream>
 
-template <typename T> void swap(T &a, T &b)
+/* Template para intercambiar valores */
+template <typename T> // Definicion que acepta cualquier tipo de dato
+void swap(T &a, T &b) // Se pasan las variables por referencia para modificarlas
 {
 	T middleman;
 
@@ -10,12 +12,15 @@ template <typename T> void swap(T &a, T &b)
 	b = middleman;
 }
 
-template <typename T> T min(T &x, T &y)
+/* Template para encontrar el minimo de dos valores */
+template <typename T>
+T min(T &x, T &y) // Se envia por referencia para no hacer copias innecesarias
 {
-	return (x < y) ? x : y;
+	return (x < y) ? x : y; // Devuelve el menor de los dos valores
 }
 
-template <typename T> T max(T &x, T &y)
+template <typename T> 
+T max(T &x, T &y)
 {
-	return (x > y) ? x : y;
+	return (x > y) ? x : y; // Devuelve el mayor de los dos valores
 }
